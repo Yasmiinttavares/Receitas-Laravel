6b0ee2f7a541c624fa11532e8c788d82f7d4ad68
Yasmiinttavares <yasmin.jesus@universo.univates.br>
2026-06-29 20:06:27 -0300

    mudando label

--- receitas-app/resources/views/receitas/create.blade.php
+++ receitas-app/resources/views/receitas/create.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Nova Receitaaaa</title>
+    <title>Nova Receitaa</title>
 </head>
 <body>
     <h1>Nova Receita</h1>
