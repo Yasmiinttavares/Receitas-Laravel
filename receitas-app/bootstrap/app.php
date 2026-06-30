ba3e268faee2fb94d5115945c868bba3b2e12345
Yasmiinttavares <yasmin.jesus@universo.univates.br>
2026-06-29 21:49:46 -0300

    troca app()->environment por getenv no bootstrap

--- receitas-app/bootstrap/app.php
+++ receitas-app/bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        if (app()->environment('testing')) {
+        if (getenv('APP_ENV') === 'testing') {
             $middleware->remove(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class);
         }
     })
